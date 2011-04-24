9589979fc3bc45589e0f0e84fab30e0d86239c6c
NateLogan <nate_logan@centrum.cz>
2011-04-24 03:20:15 +0200

    Corrected plugin.yml and formatted source code.

--- src/mn/aPunch/LightSwitch/LightSwitch.java
+++ src/mn/aPunch/LightSwitch/LightSwitch.java
@@ -7,37 +7,35 @@ import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 /**
  * LightSwitch for Bukkit
  * 
  * @author aPunch
  */
 public class LightSwitch extends JavaPlugin {
-	LightSwitchBlockListener blockListener = new LightSwitchBlockListener();
-	LightSwitchPlayerListener playerListener = new LightSwitchPlayerListener();
-	public static Logger log;
 
-	@Override
-	public void onEnable() {
-		log = Logger.getLogger("Minecraft");
-		log.info("[" + this.getDescription().getName() + "]" + " version "
-				+ this.getDescription().getVersion() + " loaded.");
+    LightSwitchBlockListener blockListener = new LightSwitchBlockListener();
+    LightSwitchPlayerListener playerListener = new LightSwitchPlayerListener();
+    public static final Logger log = Logger.getLogger("Minecraft");
 
-		// Register our events
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener,
-				Priority.Normal, this);
-		pm.registerEvent(Event.Type.REDSTONE_CHANGE, blockListener,
-				Priority.Normal, this);
-		pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener,
-				Priority.Normal, this);
-	}
+    @Override
+    public void onEnable() {
+        log.info("[" + this.getDescription().getName() + "]" + " version "
+                + this.getDescription().getVersion() + " loaded.");
 
-	@Override
-	public void onDisable() {
-		log.info("[" + this.getDescription().getName() + "]" + " version "
-				+ this.getDescription().getVersion() + " unloaded.");
-	}
+        // Register our events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener,
+                Priority.Normal, this);
+        pm.registerEvent(Event.Type.REDSTONE_CHANGE, blockListener,
+                Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener,
+                Priority.Normal, this);
+    }
 
+    @Override
+    public void onDisable() {
+        log.info("[" + this.getDescription().getName() + "]" + " version "
+                + this.getDescription().getVersion() + " unloaded.");
+    }
 }
--- src/mn/aPunch/LightSwitch/LightSwitchPlayerListener.java
+++ src/mn/aPunch/LightSwitch/LightSwitchPlayerListener.java
@@ -1,18 +1,17 @@
 package mn.aPunch.LightSwitch;
 
-
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
 
 public class LightSwitchPlayerListener extends PlayerListener {
 
-	public void onPlayerInteract(PlayerInteractEvent event) {
-		if (event.isCancelled()) {
-			return;
-		}
-		if (event.hasBlock()) {
-			LightSwitchBlockListener
-					.changePumpkinState(event.getClickedBlock());
-		}
-	}
+    @Override
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        if (event.hasBlock()) {
+            LightSwitchBlockListener.changePumpkinState(event.getClickedBlock());
+        }
+    }
 }
